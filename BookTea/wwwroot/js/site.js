ec20b4cc4dea770a83fee2fd486a92733fef2572
Mah-Atef-Abdelaziz <135140129+Mah-Atef-Abdelaziz@users.noreply.github.com>
2024-09-27 18:53:44 +0300

    Files pload

---
+++ BookTea/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(".custom-file-input").on("change", function () {
+    var fileName = $(this).val().split("\\").pop();
+    document.getElementById('PreviewPhoto').src = window.URL.createObjectURL(this.files[0]);
+    //document.getElementById('PhotoUrl').value = fileName;
+});
